fba19dc56b91204ce73a32dd810b2f923a01fc77
domespa <dumiii1988@gmail.com>
2025-02-03 15:05:37 +0100

    add form

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,44 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [articleName, setArticleName] = useState("");
+  const [articlesList, setArticlesList] = useState([
+    "Trump: Presto colpiremo economicamente l'Ue",
+    `A Bruxelles vertice informale Ue per fare il punto sulla Difesa (e i dazi)`,
+    `Scholz: "Pronti a rispondere ai dazi con i dazi"`,
+  ]);
+
+  const articleSubmit = (e) => {
+    e.preventDefault();
+
+    if (articlesList.includes(articleName)) {
+      alert(`Articolo già presente`);
+    } else {
+      setArticlesList((currentState) => [...currentState, articleName]);
+      setArticleName("");
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <h1>LISTA ARTICOLI</h1>
+        <ul>
+          {articlesList.map((article, id) => (
+            <li key={id}>{article}</li>
+          ))}
+        </ul>
+        <hr />
+        <h1>Aggiungi Articolo</h1>
+        <form onSubmit={articleSubmit}>
+          <input
+            type="text"
+            value={articleName}
+            onChange={(e) => setArticleName(e.target.value)}
+          />
+          <button type="submit">Invia</button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
